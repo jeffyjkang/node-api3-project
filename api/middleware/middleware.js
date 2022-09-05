01744deffecaca5559676f13308e0871caca32dc
jeff kang <jeffyjkang@gmail.com>
2022-09-04 22:33:02 -0500

    mvp

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,59 @@
+const { getById } = require('../users/users-model');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const logObject = {
+    method: req.method,
+    url: req.url,
+    timestamp: Date.now()
+  }
+  console.log(logObject)
+  next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const user = await getById(req.params.id)
+    if (user) {
+      req.user = user;
+      next();
+    }
+    else {
+      res.status(404).json({ message: 'user not found'})
+    }
+  }
+  catch (err) {
+    res.status(500).json({ message: 'there was a problem retrieving user'});
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  if (req.body.name) {
+    req.body.name = req.body.name.trim();
+    next();
+  }
+  else {
+    res.status(400).json({ message: 'missing required name field'});
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  if (req.body.text) {
+    req.body.text = req.body.text.trim()
+    next();
+  }
+  else {
+    res.status(400).json({ message: 'missing required text field'})
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
+const { logger } = require('./middleware/middleware');
+
+const usersRouter = require('./users/users-router');
+
 const server = express();
+server.use(express.json());
+server.use(logger);
+server.use('/api/users', usersRouter);
 
 // remember express by default cannot parse JSON in request bodies
 
